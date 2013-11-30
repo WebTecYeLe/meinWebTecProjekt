283859b628fc29cde2bdf2da40f7ac63b103d3c6
WebTecYeLe <dolebert@htwg-konstanz.de>
2013-11-30 15:17:56 +0100

    WebTecYeLe 30.11.2013 15 Uhr
    Registrierungsformular verbessert. Jetzt erfolgt auch die Überprüfung ob
    der Fahrer mindestens 18 Jahre alt ist. Demnächst wird die Eindeutigkeit
    der Email und Benutzernamen verbessert.

--- app/controllers/Registrierung.java
+++ app/controllers/Registrierung.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import java.net.UnknownHostException;
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 import play.Logger;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -19,48 +22,54 @@ public class Registrierung extends Controller {
 		return redirect("/assets/html/registrierung.html");
 
 	}
+	
+	
 
 	public static Result abschicken() {
 
 		Map<String, String[]> parameters = request().body().asFormUrlEncoded();
-		// String requestBody = request().body().asText();
-		System.out.println("hi");
+		
+		//Parameteruebergaben werden ueberprueft
 		if (!(parameters != null
 				&& parameters.containsKey("exampleInputUsername")
 				&& parameters.containsKey("exampleInputEmailRegistrierung")
 				&& parameters.containsKey("exampleInputPasswordRegistrierung")
 				&& parameters.containsKey("exampleInputPasswordRegistrierungValidierung")
-				&& parameters.containsKey("mfg_fahrer")
 				&& parameters.containsKey("exampleInputVorname")
 				&& parameters.containsKey("exampleInputName")
 				&& parameters.containsKey("Tag")
 				&& parameters.containsKey("Monat")
 				&& parameters.containsKey("Jahr")
 				&& parameters.containsKey("exampleInputTel"))) {
-			// return badRequest("go away");
+			
 			Logger.warn("bad login request");
 			return redirect("/assets/html/registrierung.html");
 		}
 
-		
+		//Parameterwerte werden ausgelesen
 		String username = parameters.get("exampleInputUsername")[0];
 		String email = parameters.get("exampleInputEmailRegistrierung")[0];
 		String password = parameters.get("exampleInputPasswordRegistrierung")[0];
 		String passwordValidierung = parameters.get("exampleInputPasswordRegistrierungValidierung")[0];
 		
-		String[] fahrer = parameters.get("mfg_fahrer");
+		String[] fahrer = parameters.get("optionsFahrer");
+		String fahrertyp;
 		
+		//Unterscheidung ob der Nutzer ein Fahrer oder Mitfahrer ist
 		if(fahrer[0].equals("Fahrer")) {
+			fahrertyp = "Fahrer";
 			
-			
+		} else {
+			fahrertyp = "Mitfahrer";
 		}
 		
-		
+		//Ueberpruefung ob das Password korrekt wiederholt eingegeben wurde
 		if(!password.equals(passwordValidierung)) {
 			return redirect("/assets/html/registrierung.html");
 			
 		}
 		
+		//Die Anrede wird ermittelt
 		String[] anrede = parameters.get("optionsRadios");
 		String titel;
 		
@@ -72,7 +81,7 @@ public class Registrierung extends Controller {
 		}
 		
 		
-		
+		//Parameterwerte werden ausgelesen
 		String tag = parameters.get("Tag")[0];
 		String monat = parameters.get("Monat")[0];
 		String jahr = parameters.get("Jahr")[0];
@@ -82,7 +91,33 @@ public class Registrierung extends Controller {
 		String name = parameters.get("exampleInputName")[0];
 		String telefon = parameters.get("exampleInputTel")[0];
 		
-
+		String alter = "";
+		int alt;
+		
+		//Das Alter wird zunaechst ermittelt
+		GregorianCalendar today = new GregorianCalendar();
+		GregorianCalendar past = new GregorianCalendar(Integer.parseInt(jahr), Integer.parseInt(monat) - 1, Integer.parseInt(tag));
+		
+		
+		long difference = today.getTimeInMillis() - past.getTimeInMillis();
+		int days = (int)(difference / (1000 * 60 * 60 * 24));
+		double years = (double)days / 365;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("");
+		sb.append(years);
+		alter = sb.toString();
+		
+		//Bestimmung des Alters als ganze Zahl
+		alt = (int) Math.floor(Double.parseDouble(alter));
+		
+		//Prueft ob der Fahrer mindestens 18 Jahre alt ist
+		if(fahrertyp.equals("Fahrer") && alt < 18) {
+			return redirect("/assets/html/registrierung.html");
+		}
+		
+		
+		//Daten werden an die Datenbank uebertragen
 		try {
 			MongoClient mongoClient = new MongoClient("localhost", 27017);
 			DB db = mongoClient.getDB("play_basics");
@@ -90,8 +125,10 @@ public class Registrierung extends Controller {
 			BasicDBObject doc = new BasicDBObject("username", username).
 					append("email", email).
 					append("password", password).
-					append("geburtsdatum", geburtsdatum).
 					append("titel", titel).
+					append("fahrertyp", fahrertyp).
+					append("geburtsdatum", geburtsdatum).
+					append("alt", alt).
 					append("vorname", vorname).
 					append("name", name).
 					append("tel", telefon);
