341f223da794ebf8296e9f15138c405a2a49b1ec
WebTecYeLe <dolebert@htwg-konstanz.de>
2013-11-30 13:03:04 +0100

    WebTecYeLe 30.11.2013 13Uhr
    Registrierungsformular verbessert, Validierungen hinzugefügt,
    Das Formular wird noch weiter verbessert

--- app/controllers/Registrierung.java
+++ app/controllers/Registrierung.java
@@ -29,9 +29,14 @@ public class Registrierung extends Controller {
 				&& parameters.containsKey("exampleInputUsername")
 				&& parameters.containsKey("exampleInputEmailRegistrierung")
 				&& parameters.containsKey("exampleInputPasswordRegistrierung")
+				&& parameters.containsKey("exampleInputPasswordRegistrierungValidierung")
+				&& parameters.containsKey("mfg_fahrer")
 				&& parameters.containsKey("exampleInputVorname")
-				&& parameters.containsKey("exampleInputName") && parameters
-					.containsKey("exampleInputTel"))) {
+				&& parameters.containsKey("exampleInputName")
+				&& parameters.containsKey("Tag")
+				&& parameters.containsKey("Monat")
+				&& parameters.containsKey("Jahr")
+				&& parameters.containsKey("exampleInputTel"))) {
 			// return badRequest("go away");
 			Logger.warn("bad login request");
 			return redirect("/assets/html/registrierung.html");
@@ -41,6 +46,38 @@ public class Registrierung extends Controller {
 		String username = parameters.get("exampleInputUsername")[0];
 		String email = parameters.get("exampleInputEmailRegistrierung")[0];
 		String password = parameters.get("exampleInputPasswordRegistrierung")[0];
+		String passwordValidierung = parameters.get("exampleInputPasswordRegistrierungValidierung")[0];
+		
+		String[] fahrer = parameters.get("mfg_fahrer");
+		
+		if(fahrer[0].equals("Fahrer")) {
+			
+			
+		}
+		
+		
+		if(!password.equals(passwordValidierung)) {
+			return redirect("/assets/html/registrierung.html");
+			
+		}
+		
+		String[] anrede = parameters.get("optionsRadios");
+		String titel;
+		
+		if(anrede[0].equals("Herr")) {
+			titel = "Herr";			
+			
+		} else {
+			titel = "Frau";
+		}
+		
+		
+		
+		String tag = parameters.get("Tag")[0];
+		String monat = parameters.get("Monat")[0];
+		String jahr = parameters.get("Jahr")[0];
+		String geburtsdatum = tag+"."+monat+"."+jahr;
+		
 		String vorname = parameters.get("exampleInputVorname")[0];
 		String name = parameters.get("exampleInputName")[0];
 		String telefon = parameters.get("exampleInputTel")[0];
@@ -53,6 +90,8 @@ public class Registrierung extends Controller {
 			BasicDBObject doc = new BasicDBObject("username", username).
 					append("email", email).
 					append("password", password).
+					append("geburtsdatum", geburtsdatum).
+					append("titel", titel).
 					append("vorname", vorname).
 					append("name", name).
 					append("tel", telefon);
