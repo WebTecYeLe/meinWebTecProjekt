60c60adad4c2346d95df953dd0680b6c9207e7d4
WebTecYeLe <dolebert@htwg-konstanz.de>
2013-11-30 19:46:04 +0100

    WebTecYeLe 30.11.2013
    Die Registrierung prüft nun ob der Username und Email eindeutig in der
    Datenbank vorkommen. Dadurch werden Duplikate vermieden. Das
    Registrierungsformular muss nur noch durch optische Hinweise verbessert
    werden.

--- app/controllers/Registrierung.java
+++ app/controllers/Registrierung.java
@@ -4,6 +4,8 @@ import java.net.UnknownHostException;
 import java.util.GregorianCalendar;
 import java.util.Map;
 
+import org.mongojack.DBCursor;
+
 import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
@@ -22,116 +24,152 @@ public class Registrierung extends Controller {
 		return redirect("/assets/html/registrierung.html");
 
 	}
-	
-	
 
 	public static Result abschicken() {
 
 		Map<String, String[]> parameters = request().body().asFormUrlEncoded();
-		
-		//Parameteruebergaben werden ueberprueft
+
+		// Parameteruebergaben werden ueberprueft
 		if (!(parameters != null
 				&& parameters.containsKey("exampleInputUsername")
 				&& parameters.containsKey("exampleInputEmailRegistrierung")
 				&& parameters.containsKey("exampleInputPasswordRegistrierung")
-				&& parameters.containsKey("exampleInputPasswordRegistrierungValidierung")
+				&& parameters
+						.containsKey("exampleInputPasswordRegistrierungValidierung")
 				&& parameters.containsKey("exampleInputVorname")
 				&& parameters.containsKey("exampleInputName")
 				&& parameters.containsKey("Tag")
 				&& parameters.containsKey("Monat")
-				&& parameters.containsKey("Jahr")
-				&& parameters.containsKey("exampleInputTel"))) {
-			
+				&& parameters.containsKey("Jahr") && parameters
+					.containsKey("exampleInputTel"))) {
+
 			Logger.warn("bad login request");
 			return redirect("/assets/html/registrierung.html");
 		}
 
-		//Parameterwerte werden ausgelesen
+		// Parameterwerte werden ausgelesen
 		String username = parameters.get("exampleInputUsername")[0];
 		String email = parameters.get("exampleInputEmailRegistrierung")[0];
 		String password = parameters.get("exampleInputPasswordRegistrierung")[0];
-		String passwordValidierung = parameters.get("exampleInputPasswordRegistrierungValidierung")[0];
-		
+		String passwordValidierung = parameters
+				.get("exampleInputPasswordRegistrierungValidierung")[0];
+
 		String[] fahrer = parameters.get("optionsFahrer");
 		String fahrertyp;
-		
-		//Unterscheidung ob der Nutzer ein Fahrer oder Mitfahrer ist
-		if(fahrer[0].equals("Fahrer")) {
+
+		// Unterscheidung ob der Nutzer ein Fahrer oder Mitfahrer ist
+		if (fahrer[0].equals("Fahrer")) {
 			fahrertyp = "Fahrer";
-			
+
 		} else {
 			fahrertyp = "Mitfahrer";
 		}
-		
-		//Ueberpruefung ob das Password korrekt wiederholt eingegeben wurde
-		if(!password.equals(passwordValidierung)) {
+
+		// Ueberpruefung ob das Password korrekt wiederholt eingegeben wurde
+		if (!password.equals(passwordValidierung)) {
 			return redirect("/assets/html/registrierung.html");
-			
+
 		}
-		
-		//Die Anrede wird ermittelt
+
+		// Die Anrede wird ermittelt
 		String[] anrede = parameters.get("optionsRadios");
 		String titel;
-		
-		if(anrede[0].equals("Herr")) {
-			titel = "Herr";			
-			
+
+		if (anrede[0].equals("Herr")) {
+			titel = "Herr";
+
 		} else {
 			titel = "Frau";
 		}
-		
-		
-		//Parameterwerte werden ausgelesen
+
+		// Parameterwerte werden ausgelesen
 		String tag = parameters.get("Tag")[0];
 		String monat = parameters.get("Monat")[0];
 		String jahr = parameters.get("Jahr")[0];
-		String geburtsdatum = tag+"."+monat+"."+jahr;
-		
+		String geburtsdatum = tag + "." + monat + "." + jahr;
+
 		String vorname = parameters.get("exampleInputVorname")[0];
 		String name = parameters.get("exampleInputName")[0];
 		String telefon = parameters.get("exampleInputTel")[0];
-		
+
 		String alter = "";
 		int alt;
-		
-		//Das Alter wird zunaechst ermittelt
+
+		// Das Alter wird zunaechst ermittelt
 		GregorianCalendar today = new GregorianCalendar();
-		GregorianCalendar past = new GregorianCalendar(Integer.parseInt(jahr), Integer.parseInt(monat) - 1, Integer.parseInt(tag));
-		
-		
+		GregorianCalendar past = new GregorianCalendar(Integer.parseInt(jahr),
+				Integer.parseInt(monat) - 1, Integer.parseInt(tag));
+
 		long difference = today.getTimeInMillis() - past.getTimeInMillis();
-		int days = (int)(difference / (1000 * 60 * 60 * 24));
-		double years = (double)days / 365;
-		
+		int days = (int) (difference / (1000 * 60 * 60 * 24));
+		double years = (double) days / 365;
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("");
 		sb.append(years);
 		alter = sb.toString();
-		
-		//Bestimmung des Alters als ganze Zahl
+
+		// Bestimmung des Alters als ganze Zahl
 		alt = (int) Math.floor(Double.parseDouble(alter));
-		
-		//Prueft ob der Fahrer mindestens 18 Jahre alt ist
-		if(fahrertyp.equals("Fahrer") && alt < 18) {
+
+		// Prueft ob der Fahrer mindestens 18 Jahre alt ist
+		if (fahrertyp.equals("Fahrer") && alt < 18) {
 			return redirect("/assets/html/registrierung.html");
 		}
-		
-		
-		//Daten werden an die Datenbank uebertragen
+
+		// Daten werden an die Datenbank uebertragen
 		try {
 			MongoClient mongoClient = new MongoClient("localhost", 27017);
 			DB db = mongoClient.getDB("play_basics");
 			DBCollection coll = db.getCollection("user");
-			BasicDBObject doc = new BasicDBObject("username", username).
-					append("email", email).
-					append("password", password).
-					append("titel", titel).
-					append("fahrertyp", fahrertyp).
-					append("geburtsdatum", geburtsdatum).
-					append("alt", alt).
-					append("vorname", vorname).
-					append("name", name).
-					append("tel", telefon);
+
+			// Uberpruefung der Eindeutigkeit von Email und Username
+			com.mongodb.DBCursor cursor = coll.find();
+			BasicDBObject query = new BasicDBObject("username", username);
+			cursor = coll.find(query);
+
+			String sucheUsername = "";
+
+			try {
+				while (cursor.hasNext()) {
+					sucheUsername += cursor.next();
+				}
+			} finally {
+				cursor.close();
+			}
+
+			// Sicherstellung dass der Username eindeutig nur einmal in der
+			// Datenbank vorkommt
+			if (!sucheUsername.isEmpty()) {
+				mongoClient.close();
+				return redirect("/assets/html/registrierung.html");
+			}
+
+			query = new BasicDBObject("email", email);
+			cursor = coll.find(query);
+
+			String sucheEmail = "";
+
+			try {
+				while (cursor.hasNext()) {
+					sucheEmail += cursor.next();
+				}
+			} finally {
+				cursor.close();
+			}
+
+			// Email darf nur einmal in der Datenbank vorkommen
+			if (!sucheEmail.isEmpty()) {
+				mongoClient.close();
+				return redirect("/assets/html/registrierung.html");
+			}
+
+			BasicDBObject doc = new BasicDBObject("username", username)
+					.append("email", email).append("password", password)
+					.append("titel", titel).append("fahrertyp", fahrertyp)
+					.append("geburtsdatum", geburtsdatum).append("alt", alt)
+					.append("vorname", vorname).append("name", name)
+					.append("tel", telefon);
 			coll.insert(doc);
 			mongoClient.close();
 		} catch (UnknownHostException e) {
